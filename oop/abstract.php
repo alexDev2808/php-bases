449a37e8eb1ec83642669c595bd6c192b73b74c2
J Alexis <colores.2808.8000@gmail.com>
2026-03-10 07:45:34 -0600

    ✨ Clases: Interfaces

--- oop/abstract.php
+++ oop/abstract.php
@@ -1,17 +1,5 @@
 <?php
 
-$beer = new Beer("Lager", 10.00);
-echo $beer->getName();
-echo "<br>";
-
-showInfo($beer);
-
-function showInfo(Product $product) {
-    echo "Product: " . $product->getName() . "<br>";
-    echo "Price with tax: " . $product->calculatePrice() . "<br>";
-}
-
-
 abstract class Product {
     protected float $price;
     protected string $name;
@@ -35,3 +23,62 @@ class Beer extends Product {
     }
 
 }
+
+$beer = new Beer("Lager", 10.00);
+echo $beer->getName();
+echo "<br>";
+
+showInfo($beer);
+
+function showInfo(Product $product) {
+    echo "Product: " . $product->getName() . "<br>";
+    echo "Price with tax: " . $product->calculatePrice() . "<br>";
+}
+
+// Interfaces
+
+interface SendInterface {
+    public function send(string $message): void;
+}
+
+interface SaveInterface {
+    public function save(): void;
+}
+
+
+class Document implements SendInterface, SaveInterface {
+    public function send(string $message): void {
+        echo "Sending message: " . $message . "<br>";
+    }
+
+    public function save(): void {
+        echo "Document saved.<br>";
+    }
+}
+
+class BeerRepository implements SaveInterface {
+    public function save(): void {
+        echo "Beer saved in the repository.<br>";
+    }
+}
+
+class SaveProgress {
+    private SaveInterface $saveManager;
+    
+    public function __construct(SaveInterface $saveManager)
+    {
+        $this->saveManager = $saveManager;
+    }
+
+    public function keep() {
+        echo "Hacemos algo antes de guardar...<br>";
+        $this->saveManager->save();
+    }
+
+}
+
+$beerRepository = new BeerRepository();
+$document = new Document();
+$saveProgress = new SaveProgress($beerRepository);
+$saveProgress->keep();
+
